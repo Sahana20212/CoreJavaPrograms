9e9019c817cca459812fd15a88d41dc7f26c440a
Rama <ramadevi.at.2020@gmail.com>
2023-10-14 16:13:30 -0400

    Added comments

--- src/com/java/SumofNumUsing8.java
+++ src/com/java/SumofNumUsing8.java
@@ -6,12 +6,12 @@ import java.util.List;
 public class SumofNumUsing8 {
 	
 	public static void main (String args[]) {
-		
+		//IntegersumIdid
 		List<Integer> list = Arrays.asList(1,4,5,6,7,12,3,4,5,6);
 		System.out.println(list.stream().mapToInt(i->i).sum());
-		
+		//Interger find min num.
 		System.out.println(list.stream().mapToInt(n->n).min().getAsInt());
-		
+		////Interger find max num.
 		System.out.println(list.stream().mapToInt(n->n).max().getAsInt());
 	}
 
